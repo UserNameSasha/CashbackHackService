13f468d822c33c73e7f316023f9a57fb006707ce
user test <klimchuk.alieksandr.1987@mail.ru>
2023-09-09 09:16:29 +0300

    change Test

---
+++ src/test/java/CashbackHackServiceTest/CashbackHackServiceTest.java
@@ -0,0 +1,33 @@
+package CashbackHackServiceTest;
+
+import org.testng.annotations.Test;
+import ru.netology.unit.CashbackHackService;
+
+import static org.testng.Assert.*;
+
+
+public class CashbackHackServiceTest {
+
+      @Test
+    public void testIfLess1000() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 800;
+        int actual = service.remain(200);
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testIfEquals1000() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1000;
+        int actual = service.remain(0);
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void testIfMore1000() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(1100);
+        assertEquals(actual, expected);
+    }
+
+}
